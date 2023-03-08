19e790474032985a8f9dc0029bccac1249b7735d
jonyjjo <jony_jjo@hotmail.com>
2023-03-08 04:33:58 +0000

    update product model and reducers

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,15 +1,13 @@
 export interface Product {
-    id: number;
-    title: string;
-    description: string;
-    price: number;
-    discountPercentage: number;
-    rating : number;
-    stock : number;
-    brand : string;
-    category : string;
-    thumbnail : string;
-    images : [ 
-        url: string 
-    ];
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: Array<string>;
 }
